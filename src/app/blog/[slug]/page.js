299a928cefc3e82309f3152b7c1093be03f5b727
lyapkin <lyapkin992@gmail.com>
2024-04-11 12:30:03 +0500

    .

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,13 +1,16 @@
 import BlogService from "@/services/BlogService";
 
+import styles from '@/styles/blog.module.css'
+
 export default async function BlogPost({ params }) {
     const content = await BlogService.getArticleContent(params.slug);
 
     return (
-        <div className={`first-screen`}>
+        <div className={`first-screen ${styles['blog-inside']}`}>
             <div className="container">
                 {!content.detail ? (
                     <div
+                        className={styles['blog-inside__content']}
                         dangerouslySetInnerHTML={{ __html: content.content }}
                     ></div>
                 ) : (
