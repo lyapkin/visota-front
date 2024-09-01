06e283ee65ecca4b7bb53d49ebe0a29adcab4d23
lyapkin <lyapkin992@gmail.com>
2024-09-01 14:58:48 +0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "http://81.177.140.109/api",
-    BACK_URL: "http://81.177.140.109",
+    API_URL: "https://visota13.ru/api",
+    BACK_URL: "https://visota13.ru",
     BASE_URL: "http://localhost:3000",
 
     // API_URL: 'http://localhost:8000/api',
