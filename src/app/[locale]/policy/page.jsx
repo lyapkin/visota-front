c150c72cb7fca0d7942508876425ac7eb40e2e91
lyapkin <lyapkin992@gmail.com>
2024-09-01 16:07:44 +0500

    .

--- src/app/[locale]/policy/page.jsx
+++ src/app/[locale]/policy/page.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+import s from "@/styles/policy.module.css";
+
 import initTranslations from "@/locales/i18n";
 
 const Policy = async ({ params: { locale } }) => {
   const { t } = await initTranslations(locale, ["policy"]);
   return (
     <div
-      className="container first-screen"
+      className={`${s.policy} container first-screen`}
       dangerouslySetInnerHTML={{
         __html: t("policy:private", { interpolation: { escapeValue: false } }),
       }}
